8c5be9af6baee20b2e01f43b25f6e309d068e585
Brandon White <brandonkwhite3@gmail.com>
2023-03-29 01:01:08 -0400

    Forgot the savedata line in kanbanAPI at line 35. Gotta remember to save entries..

--- js/api/KanBanAPI.js
+++ js/api/KanBanAPI.js
@@ -1,159 +1,14 @@
-// // The backend for the KanBan this will allow the kanban to work with local storage for storing data
+// The backend for the KanBan this will allow the kanban to work with local storage for storing data
 
 
-// // The class to export 
-// export default class KanbanAPI {
-//     // the method to show a column where the data is/will be stored.
-//     static getItems(columnId) {
-//         const column = read().find(column => column.id == columnId)
-
-
-//         // an error case
-//         if (!column) {
-//             return []
-//         }
-
-//         return column.items
-//     }
-
-//     //Creating new items to be stored in the column.
-//     static insertItem(columnId, content) {
-//         const data = read()
-//         const column = data.find(column => column.id == columnId)
-//         const item = {
-//             // Creete an id for items by using a random number 0 - 100000, and rounding it to whole numbers, and the content that will exist in the item.
-//             id: Math.floor(Math.random() * 100000),
-//             content,
-//         }
-
-//         if (!column) {
-//             throw new Error("Column does not exist.")
-//         }
-
-//         //Add the item to the column, and save the entry. 
-//         column.items.push(item)
-//         // save(data)
-
-//         // Ensure the new item will be displayed. 
-//         return item
-//     }
-
-//     static updateItem(itemId, newProps) {
-//         const data = read()
-//         // Perform a little destructuring on the item and current column, by creating an anon function and assigning results to those variables.
-//         const [item, currentColumn] = (() => {
-//             // looping through the the coluns of data 
-//             for (const column of data) {
-//                 const item = column.items.find(item => item.id == itemId)
-
-//                 // If you found that item, then get back the current item info and it's column
-//                 if (item) {
-//                     return [item, column]
-//                 }
-//             }
-//         })()
-
-//         if (!item) {
-//             throw new Error("Item not found!")
-//         }
-
-
-//         // Check to see if the item you're looking at has had updated it's content after a move or whatever, and whether it needs to be reset. 
-//         item.content = newProps.content === undefined ? item.content : newProps.content
-
-//         // Updating column item positions
-//         if (
-//             // If there's a new item in a new column, or a new position for an item
-//             newProps.columnId !== undefined && newProps.position !== undefined
-//         ) {
-//             const targetColumn = data.find(column => column.id == newProps.columnId)
-
-//             if (!targetColumn) {
-//                 throw new Error("Target column not found!")
-//             }
-
-//             // Delete the item out of it's old column
-//             currentColumn.items.splice(currentColumn.items.indexOf(item), 1)
-
-//             // Place that column item, into the new column/positon
-//             targetColumn.items.splice(newProps.position, 0, item)
-//         }
-//         save(data)
-
-//     }
-
-//     // })
-//     // }
-
-//     // Delete an item
-//     static deleteItem(itemId) {
-//         const data = read()
-
-//         // Loop through the columns of items
-//         for (const column of data) {
-//             // Loop through the items in the column
-//             const item = column.items.find(item => item.id == itemId)
-
-//             // If the item exists, want to adapt the list with a splice
-//             if (item) {
-//                 column.items.splice(column.items.indexOf(item), 1)
-//             }
-//         }
-
-//         save(data)
-//     }
-// }
-
-
-// // A function to read info from the local storage
-// function read() {
-//     const json = localStorage.getItem("kanban-data")
-
-//     // If there is no data in local storage, then I want to return a default array of columns that will accept our items.
-//     if (!json) {
-//         return [
-
-//             {
-//                 id: 1,
-//                 items: []
-//             },
-//             {
-//                 id: 2,
-//                 items: []
-//             },
-//             {
-//                 id: 3,
-//                 items: []
-//             },
-//             {
-//                 id: 4,
-//                 items: []
-//             },
-
-//         ]
-//     }
-
-//     // return that data as readable JSON 
-//     return JSON.parse(json)
-// }
-
-
-// // Put stuff into the local storage
-// function save(data) {
-
-//     // Have to send data to localStorage as a string; don't forget JSON.stringify.
-//     localStorage.setItem
-//         ("kanban-data", JSON.stringify(data))
-// }
-
-
-
-
-
+// The class to export
 export default class KanbanAPI {
+    // the method to show a column where the data is/will be stored.
     static getItems(columnId) {
         const column = read().find(column => column.id == columnId)
 
+
+        // an error case
         if (!column) {
             return []
         }
@@ -161,21 +16,25 @@ export default class KanbanAPI {
         return column.items
     }
 
+    //Creating new items to be stored in the column.
     static insertItem(columnId, content) {
         const data = read()
         const column = data.find(column => column.id == columnId)
         const item = {
+            // Creete an id for items by using a random number 0 - 100000, and rounding it to whole numbers, and the content that will exist in the item.
             id: Math.floor(Math.random() * 100000),
             content,
         }
 
         if (!column) {
-            throw new Error("Column does not exist")
+            throw new Error("Column does not exist.")
         }
 
+        //Add the item to the column, and save the entry.
         column.items.push(item)
-        // save(data)
+        save(data)
 
+        // Ensure the new item will be displayed.
         return item
     }
 
@@ -183,44 +42,54 @@ export default class KanbanAPI {
         const data = read()
         const [item, currentColumn] = (() => {
             for (const column of data) {
-                const item = column.items.find(item => item.id === itemId)
+                const item = column.items.find(item => item.id == itemId)
 
                 if (item) {
                     return [item, column]
                 }
             }
-        })
+        })()
 
         if (!item) {
-            throw new Error("Item not found")
+            throw new Error("Item not found!")
         }
 
+
+        // Check to see if the item you're looking at has had updated it's content after a move or whatever, and whether it needs to be reset.
         item.content = newProps.content === undefined ? item.content : newProps.content
 
+        // Updating column item positions
         if (
-            newProps.columnId !== undefined
-            && newProps.position !== undefined
+            // If there's a new item in a new column, or a new position for an item
+            newProps.columnId !== undefined && newProps.position !== undefined
         ) {
             const targetColumn = data.find(column => column.id == newProps.columnId)
 
             if (!targetColumn) {
-                throw new Error("Target column not found")
+                throw new Error("Target column not found!")
             }
 
+            // Delete the item out of it's old column
             currentColumn.items.splice(currentColumn.items.indexOf(item), 1)
 
+            // Place that column item, into the new column/positon
             targetColumn.items.splice(newProps.position, 0, item)
         }
-
         save(data)
+
     }
 
+
+    // Delete an item
     static deleteItem(itemId) {
         const data = read()
 
+        // Loop through the columns of items
         for (const column of data) {
+            // Loop through the items in the column
             const item = column.items.find(item => item.id == itemId)
 
+            // If the item exists, want to adapt the list with a splice
             if (item) {
                 column.items.splice(column.items.indexOf(item), 1)
             }
@@ -230,11 +99,15 @@ export default class KanbanAPI {
     }
 }
 
+
+// A function to read info from the local storage
 function read() {
     const json = localStorage.getItem("kanban-data")
 
+    // If there is no data in local storage, then I want to return a default array of columns that will accept our items.
     if (!json) {
         return [
+
             {
                 id: 1,
                 items: []
@@ -251,12 +124,20 @@ function read() {
                 id: 4,
                 items: []
             },
+
         ]
     }
 
+    // return that data as readable JSON
     return JSON.parse(json)
 }
 
+
+// Put stuff into the local storage
 function save(data) {
-    localStorage.setItem("kanban-data", JSON.stringify(data))
+
+    // Have to send data to localStorage as a string; don't forget JSON.stringify.
+    localStorage.setItem
+        ("kanban-data", JSON.stringify(data))
 }
+
